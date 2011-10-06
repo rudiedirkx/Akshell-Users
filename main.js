e543ee0210748ac9f8b1d28251f0a3b977368072
rudiedirkx <akshell@hotblocks.nl>
2011-10-06 23:17:41 +0000

    show all, controller names

--- main.js
+++ main.js
@@ -27,20 +27,27 @@ var IndexHandler = Handler.subclass({
 
 var UserHandler = Handler.subclass({
   get: function(request, page) {
-    page || (page = 1);
+    var per_page = 2
+    if ( 'all' === page ) {
+      per_page = 99999
+      page = 1
+    }
+    else {
+      page || (page = 1)
+    }
 
     var num_users = db.User.where('1').count();
-    var pages = Math.ceil(num_users / 2);
+    var pages = Math.ceil(num_users / per_page);
 
     return render('users.html', {
       page: page,
       pages: pages,
-      users: db.User.all().get({start: 2 * (page-1), length: 2, by: 'name'})
+      users: db.User.all().get({start: per_page * (page-1), length: per_page, by: 'name'})
     });
   }
 });
 
-var UserAddHandler = Handler.subclass({
+var AddUserHandler = Handler.subclass({
   get: function(request, email) {
     email || (email = '');
     return render('adduser.html', {
@@ -57,9 +64,10 @@ var UserAddHandler = Handler.subclass({
 // Router
 exports.root = new URLMap(IndexHandler,
   ['users/', UserHandler,
-    ['add/', UserAddHandler,
-      [/([^/]+)/, UserAddHandler]
+    ['add/', AddUserHandler,
+      [/([^/]+)/, AddUserHandler]
     ],
+    [/(all)/, UserHandler],
     [/page\/(\d+)/, UserHandler]
-  ]
+]
 );
